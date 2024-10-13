6fd3791573da7cad14ed1496f1124cf29d3574dd
Davi Brito <daviibrito111@gmail.com>
2024-10-13 01:58:17 -0300

    feat: add goal completion functionality

--- src/components/PendingGoals.tsx
+++ src/components/PendingGoals.tsx
@@ -1,15 +1,25 @@
 import { Plus } from 'lucide-react'
 import { OutlineButton } from './ui/outline-button'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { getPendingGoals } from '../http/get-pending-goals'
+import { createGoalCompletion } from '../http/create-goal-completion'
 
 export function PendingGoals() {
+    const queryClient = useQueryClient()
+
     const { data } = useQuery({
         queryKey: ['pending-goals'],
         queryFn: getPendingGoals,
     })
 
     if (!data) return null
+
+    async function handleCompleteGoal(goalId: string) {
+        await createGoalCompletion(goalId)
+
+        queryClient.invalidateQueries({ queryKey: ['summary'] })
+        queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+    }
 
     return (
         <div className="flex flex-wrap gap-3">
@@ -18,6 +28,7 @@ export function PendingGoals() {
                     <OutlineButton
                         key={goal.id}
                         disabled={goal.completionCount >= goal.weeklyFrequency}
+                        onClick={() => handleCompleteGoal(goal.id)}
                     >
                         <Plus className="size-4 text-zinc-600" />
                         {goal.title}
